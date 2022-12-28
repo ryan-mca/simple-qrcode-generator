d1f5bb6e263dfa555634d6b891a9ce3ee5f7ae79
Ryan McAlpine <mcalpine06@outlook.com>
2022-12-28 17:06:08 +1100

    Finished the skeleton for 'wifi_qr_code'

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 """
 # --- Imports ---
 import customtkinter as ctk
-import qrcode as qrc
+import qrcode
 
 # --- Constants ---
 win = ctk.CTk()
@@ -14,7 +14,10 @@ ctk.set_default_color_theme("dark-blue")
 ctk.set_appearance_mode("dark")
 
 main_frame = ctk.CTkFrame(master=win)
+wifi_qr_code_frame = ctk.CTkFrame(master=win)
 
+file_or_show_cbox = ctk.CTkComboBox(master=wifi_qr_code_frame, values=["Save to File", "Show"])
+sec_type_cbox = ctk.CTkComboBox(master=wifi_qr_code_frame, values=["WEP", "WPA", "None"])
 cbox = ctk.CTkComboBox(master=main_frame, values=["Regular QR-Code", "WiFi QR-Code"],
                         font=("Helvetica", 11))
 
@@ -41,12 +44,38 @@ def main():
     win.mainloop()
 
 def cbox_logic():
+    """Checks the users choice and runs the respective function
+    """
     choice = cbox.get()
 
     if choice == "WiFi QR-Code":
-        return
+        main_to_wifi_qr_code()
     elif choice == "Regular QR-Code":
         return
+
+def main_to_wifi_qr_code():
+    """Gets rid of the main pages widgets and runs the 'wifi_qr_code' function
+    """
+    main_frame.pack_forget()
+
+    wifi_qr_code()
+
+def wifi_qr_code():
+    """Shows all the wifi_qr_code widgets
+    """
+    wifi_qr_code_label = ctk.CTkLabel(master=wifi_qr_code_frame, text="WiFi QR-Code", font=("Helvetica", 24))
+    wifi_ssid_input = ctk.CTkEntry(master=wifi_qr_code_frame, placeholder_text="WiFi SSID")
+    wifi_passwd_input = ctk.CTkEntry(master=wifi_qr_code_frame, placeholder_text="WiFi Password", show="*")
+    generate_qr_button = ctk.CTkButton(master=wifi_qr_code_frame, text="Generate")
+
+    wifi_qr_code_label.pack(pady=12, padx=10)
+    wifi_ssid_input.pack(pady=6, padx=10)
+    wifi_passwd_input.pack(pady=6, padx=10)
+    sec_type_cbox.pack(pady=6, padx=10)
+    generate_qr_button.pack(pady=12, padx=10)
+    sec_type_cbox.set("Security Type")
+
+    wifi_qr_code_frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 if __name__ == "__main__":
     main()
