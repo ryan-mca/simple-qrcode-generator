ae1a5a9a4de5ba1190559218e7881b468328ba3b
Ryan McAlpine <mcalpine06@outlook.com>
2022-12-28 14:07:19 +1100

    placeholder window works

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 """Handles the main customtkinter stuff
 """
+# --- Imports ---
 import customtkinter as ctk
 import qrcode as qrc
 
+# --- Constants ---
+win = ctk.CTk()
+
+# --- Functions ---
 def main():
-    return 0
+    win.mainloop()
 
 if __name__ == "__main__":
     main()
