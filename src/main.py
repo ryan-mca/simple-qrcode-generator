69c0f4362486ac483d0da67185bc67ad948350c4
Ryan McAlpine <mcalpine06@outlook.com>
2022-12-30 17:34:40 +1100

    removed 'create_qr()', started using lambda for 'CTkButtons', mostly finished 'reg_qr_code()'

--- src/main.py
+++ src/main.py
@@ -7,9 +7,6 @@ from qr_logic import gen_wifi_qr_code, gen_reg_qr_code
 # --- Constants ---
 win = ctk.CTk()
 
-# --- Extra Variables ---
-qr_type = None
-
 # --- CustomTkinter stuff ---
 # Window settings
 win.title("Simple QR-Code Generator")
@@ -25,12 +22,9 @@ cbox = ctk.CTkComboBox(master=main_frame, values=["Regular QR-Code WIP", "WiFi Q
 # Used in 'wifi_qr_code()'
 wifi_qr_code_frame = ctk.CTkFrame(master=win)
 #file_or_show_cbox = ctk.CTkComboBox(master=wifi_qr_code_frame, values=["Save to File", "Show"]) WIP
-sec_type_cbox = ctk.CTkComboBox(master=wifi_qr_code_frame, values=["WEP", "WPA", "None"])
-wifi_ssid_input = ctk.CTkEntry(master=wifi_qr_code_frame, placeholder_text="WiFi SSID")
-wifi_passwd_input = ctk.CTkEntry(master=wifi_qr_code_frame, placeholder_text="WiFi Password", show="*")
 
 # Used in 'reg_qr_code()'
-reg_qr_code_frame = ctk.CTkFrame(master=win)
+reg_qr_frame = ctk.CTkFrame(master=win)
 
 # --- Functions ---
 def main():
@@ -58,30 +52,34 @@ def cbox_logic():
     """Checks the users choice and runs the respective function
     """
     choice = cbox.get()
-    main_frame.pack_forget()
 
     if choice == "WiFi QR-Code":
+        main_frame.pack_forget()
         wifi_qr_code()
     elif choice == "Regular QR-Code":
+        main_frame.pack_forget()
         reg_qr_code()
 
 def reg_qr_code():
     """Shows all the reg_qr_code widgets
     """
-    qr_type = "regular"
+    title = ctk.CTkLabel(master=reg_qr_frame, text="Regular QR-Code", font=("Helvetica", 24))
+    text_input = ctk.CTkEntry(master=reg_qr_frame, placeholder_text="Text / URL")
+    submit_button = ctk.CTkButton(master=reg_qr_frame, text="Generate", command=lambda: gen_reg_qr_code(text_input.get()))
 
-    reg_qr_code_label = ctk.CTkLabel(master=reg_qr_code_frame, text="Regular QR-Code", font=("Helvetica", 24))
-
-    reg_qr_code_label.pack(pady=12, padx=10)
-    reg_qr_code_frame.pack(pady=20, padx=60, fill="both", expand=True)
+    title.pack(pady=12, padx=10)
+    text_input.pack(pady=12, padx=10)
+    submit_button.pack(pady=12, padx=10)
+    reg_qr_frame.pack(pady=20, padx=60, fill="both", expand=True)
 
 def wifi_qr_code():
     """Shows all the wifi_qr_code widgets
     """
-    qr_type = "wifi"
-
+    sec_type_cbox = ctk.CTkComboBox(master=wifi_qr_code_frame, values=["WEP", "WPA", "None"])
+    wifi_ssid_input = ctk.CTkEntry(master=wifi_qr_code_frame, placeholder_text="WiFi SSID")
+    wifi_passwd_input = ctk.CTkEntry(master=wifi_qr_code_frame, placeholder_text="WiFi Password", show="*")
     wifi_qr_code_label = ctk.CTkLabel(master=wifi_qr_code_frame, text="WiFi QR-Code", font=("Helvetica", 24))
-    gen_qr_button = ctk.CTkButton(master=wifi_qr_code_frame, text="Generate", command=create_qr)
+    gen_qr_button = ctk.CTkButton(master=wifi_qr_code_frame, text="Generate", command=lambda: gen_wifi_qr_code(wifi_ssid_input.get(), wifi_passwd_input.get(), sec_type_cbox.get()))
 
     wifi_qr_code_label.pack(pady=12, padx=10)
     wifi_ssid_input.pack(pady=6, padx=10)
@@ -91,17 +89,6 @@ def wifi_qr_code():
     sec_type_cbox.set("Security Type")
 
     wifi_qr_code_frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-def create_qr():
-    """Checks the type of QR-Code and creates the respective type
-    """
-    if qr_type == "wifi":
-        gen_wifi_qr_code(wifi_ssid_input.get(), wifi_passwd_input.get(), sec_type_cbox.get())
-        qr_type = None
-
-    elif qr_type == "regular":
-        gen_reg_qr_code()
-        qr_type = None
 
 if __name__ == "__main__":
     main()
